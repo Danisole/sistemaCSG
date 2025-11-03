5791e2e5ab7b60a45713d19a5ef862e45d8f0885
Danisole <danisole82@gmail.com>
2025-11-03 01:39:02 -0300

    boton cerrar

---
+++ turnos_pacientes.js
@@ -0,0 +1,30 @@
+ // 🔐 Validar sesión activa
+    const token = localStorage.getItem("token");
+    if (!token) {
+        window.location.href = "login.html";
+    }
+
+    // 🚪 Cerrar sesión
+    document.getElementById("logoutBtn").addEventListener("click", () => {
+        Swal.fire({
+        title: "¿Cerrar sesión?",
+        text: "Se cerrará tu sesión actual.",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Sí, salir",
+        cancelButtonText: "Cancelar",
+        reverseButtons: true
+        }).then((result) => {
+        if (result.isConfirmed) {
+            localStorage.removeItem("token");
+            Swal.fire({
+            icon: "success",
+            title: "Sesión cerrada",
+            showConfirmButton: false,
+            timer: 1000
+            }).then(() => {
+            window.location.href = "login.html";
+            });
+        }
+        });
+    });
